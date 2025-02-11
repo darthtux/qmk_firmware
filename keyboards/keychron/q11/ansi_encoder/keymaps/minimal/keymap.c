3c8f6c7f12e4f6c399033e1c0a55042254b79f92
Edward Munsell <emunsell@gmail.com>
2025-02-10 21:13:02 -0600

    update RGB and modifiers

--- keyboards/keychron/q11/ansi_encoder/keymaps/minimal/keymap.c
+++ keyboards/keychron/q11/ansi_encoder/keymaps/minimal/keymap.c
@@ -20,6 +20,7 @@ enum layers{
     Movement,
     Numbers,
     Symbols,
+    Shortcuts,
 };
 
 #define KC_TASK LGUI(KC_TAB)
@@ -30,9 +31,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MUTE,  KC_ESC,   KC_BRID,            KC_BRIU,  KC_MCTL,  KC_LPAD,  RM_VALD,   RM_VALU,  KC_MPRV,            KC_MPLY,  KC_MNXT,  KC_MUTE,     KC_VOLD,    KC_VOLU,  KC_INS,   KC_DEL,   KC_MUTE,
         RM_TOGG,  KC_GRV,   KC_1,               KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,               KC_8,     KC_9,     KC_0,        KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
         RM_NEXT,  KC_TAB,   KC_Q,               KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,               KC_I,     KC_O,     KC_P,        KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
-        RM_PREV,  KC_CAPS,  LT(Movement,KC_A),  KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     LT(Symbols,KC_J),   KC_K,     KC_L,     KC_SCLN,     KC_QUOT,              KC_ENT,             KC_HOME,
+        RM_PREV,  KC_CAPS,  LT(Movement,KC_A),  KC_S,     KC_D,     LT(Shortcuts,KC_F),  KC_G,     KC_H,     LT(Symbols,KC_J),   KC_K,     KC_L,     KC_SCLN,     KC_QUOT,              KC_ENT,             KC_HOME,
         RM_VALU,  KC_LSFT,                      KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_N,               KC_M,     KC_COMM,  KC_DOT,      KC_SLSH,              KC_RSFT,  KC_UP,
-        RM_VALD,  KC_LCTL,  MO(Symbols),        KC_LOPT,  KC_LCMD,       KC_SPC,                        LT(Numbers,KC_ENT),      KC_RCMD,  MO(Symbols), KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT),
+        RM_VALD,  KC_LCTL,  MO(Symbols),        KC_LOPT,  KC_LCMD,       MT(MOD_LGUI,KC_SPC),                        LT(Numbers,KC_ENT),      KC_RCMD,  MO(Symbols), KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT),
 
  
     [Numbers] = LAYOUT_91_ansi(
@@ -58,6 +59,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  KC_LCBR,  KC_RCBR,  KC_GRAVE,   KC_QUES,    _______,     _______,    _______,    _______,    _______,   _______,   _______,              _______,             _______,
         _______,  _______,  KC_LABK,  KC_RABK,  KC_PLUS,    KC_UNDS,    _______,     _______,    _______,    _______,   _______,   _______,              _______,  _______,
         _______,  _______,  _______,  _______,  _______,        _______,                         _______,             _______, _______, _______,  _______,  _______,  _______),
+
+    [Shortcuts] = LAYOUT_91_ansi(
+        _______,  _______,  _______,  _______,  _______,    _______,    _______,     _______,    _______,    _______,    _______,   _______,   _______,    _______,  _______,  _______,   _______,
+        _______,  _______,  _______,  _______,  _______,    _______,    _______,     _______,    _______,    _______,    _______,   _______,   _______,    _______,  _______,             _______,
+        _______,  _______,  _______,  _______,  _______,   _______,    _______,     _______,    _______,    _______,    _______,   _______,   _______,    _______,  _______,             _______,
+        _______,  _______,  _______,  _______,  _______,   _______,    _______,     _______,    LALT(KC_BSPC),    _______,    _______,   _______,   _______,              _______,             _______,
+        _______,  _______,  _______,  _______,  _______,    _______,    _______,     _______,    _______,    _______,   _______,   _______,              _______,  _______,
+        _______,  _______,  _______,  _______,  _______,        _______,                         _______,             _______, _______, _______,  _______,  _______,  _______),
+
 };
 
 #if defined(ENCODER_MAP_ENABLE)
@@ -65,6 +75,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [MAC_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [Movement]   = { ENCODER_CCW_CW(RM_VALD, RM_VALU), ENCODER_CCW_CW(RM_VALD, RM_VALU) },
     [Numbers] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [Symbols]   = { ENCODER_CCW_CW(RM_VALD, RM_VALU), ENCODER_CCW_CW(RM_VALD, RM_VALU) }
+    [Symbols]   = { ENCODER_CCW_CW(RM_VALD, RM_VALU), ENCODER_CCW_CW(RM_VALD, RM_VALU) },
+    [Shortcuts]   = { ENCODER_CCW_CW(RM_VALD, RM_VALU), ENCODER_CCW_CW(RM_VALD, RM_VALU) }
 };
 #endif // ENCODER_MAP_ENABLE
