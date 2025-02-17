9ba522363c93ab515f0903de9a34842173969796
Edward Munsell <emunsell@umn.edu>
2025-02-17 11:59:37 -0600

    fix typo

--- keyboards/keychron/q11/ansi_encoder/keymaps/minimalCombo/keymap.c
+++ keyboards/keychron/q11/ansi_encoder/keymaps/minimalCombo/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MUTE,  KC_ESC,   KC_BRID,            KC_BRIU,  KC_MCTL,  KC_LPAD,  RM_VALD,   RM_VALU,  KC_MPRV,             KC_MPLY,             KC_MNXT,  KC_MUTE,     KC_VOLD,    KC_VOLU,  KC_INS,   KC_DEL,   KC_MUTE,
         RM_TOGG,  KC_GRV,   KC_1,               KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,                KC_8,                KC_9,     KC_0,        KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
         RM_NEXT,  KC_ESC,   KC_Q,               KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,                KC_I,                KC_O,     KC_P,        KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
-        RM_PREV,  KC_TAB,   KC_A,               KC_S,     KC_Z,    KC_F,     KC_G,      KC_H,     LT(Symbols,KC_J),    LT(Shortcuts,KC_K),  KC_L,     KC_SCLN,     KC_QUOT,              KC_ENT,             KC_HOME,
+        RM_PREV,  KC_TAB,   KC_A,               KC_S,     KC_D,    KC_F,     KC_G,      KC_H,     LT(Symbols,KC_J),    LT(Shortcuts,KC_K),  KC_L,     KC_SCLN,     KC_QUOT,              KC_ENT,             KC_HOME,
         RM_VALU,  KC_LSFT,                      KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_N,                KC_M,                MT(MOD_LGUI, KC_COMM),  MT(MOD_LALT, KC_DOT),      MT(MOD_LCTL, KC_SLSH),              KC_RSFT,  KC_UP,
         RM_VALD,  KC_LCTL,  MO(Symbols),        KC_LOPT,  KC_LCMD,       MT(MOD_LSFT,KC_SPC),                           KC_ENT,              KC_RCMD,  MO(Symbols), KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT),
 
